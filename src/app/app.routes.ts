119b20ccebbcd88491f1ba1c1d6e716cb8e64d04
Giga323 <nikitanagla@gmail.com>
2024-10-12 16:07:25 +0300

    defining routes with dynamic loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadComponent: () => import('./components/home/home.component').then(
+            m => m.HomeComponent
+        )
+    },
+    {
+        path: 'detail-info',
+        loadComponent: () => import('./components/detail-info/detail-info.component').then(
+            m => m.DetailInfoComponent
+        )
+    },
+    {
+        path: 'favorites',
+        loadComponent: () => import('./components/favorites/favorites.component').then(
+            m => m.FavoritesComponent
+        )
+    },
+    {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    }
+];
