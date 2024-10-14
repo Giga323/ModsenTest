41ba4265c338a2ab25fb00f511cb03ed092db550
Giga323 <nikitanagla@gmail.com>
2024-10-14 15:35:52 +0300

    Added html-template for header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    RouterModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
 })
 export class HeaderComponent {
 
+  checkIsHomeRoute(event: any) {
+    return event
+  }
 }
