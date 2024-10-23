c697ebda0979f3233d5a8d3cf6edef0bb7a35274
Giga323 <nikitanagla@gmail.com>
2024-10-23 22:24:32 +0300

    added burger-menu to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HomeService } from '@app/services/home/home.service';
+import { BurgerMenuComponent } from '../burger-menu/burger-menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+    BurgerMenuComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
