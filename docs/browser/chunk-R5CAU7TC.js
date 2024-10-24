ea492721956689c31c087acd829cacbe9c0c0548
Giga323 <nikitanagla@gmail.com>
2024-10-24 20:24:01 +0300

    deployed project

---
+++ docs/browser/chunk-R5CAU7TC.js
@@ -0,0 +1,1 @@
+import{c as e,h as n}from"./chunk-WAWHM7R6.js";var s=(()=>{class t{constructor(){this.isHomeComponent$=new e}changeIsHomeComponent(o){this.isHomeComponent$.next(!o)}static{this.\u0275fac=function(r){return new(r||t)}}static{this.\u0275prov=n({token:t,factory:t.\u0275fac,providedIn:"root"})}}return t})();export{s as a};
